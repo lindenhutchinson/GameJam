622afbd4043356bdd65e77bf8163887953594fec
code why <45314796+BreakRyo@users.noreply.github.com>
2024-01-08 19:02:43 +0100

    attack

---
+++ PracticeGame/ProjectFolder/Nodes/FireBall.gd
@@ -0,0 +1,24 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	position = get_tree().get_first_node_in_group("player").position
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_visible_on_screen_notifier_2d_screen_exited():
+	queue_free()
+
+
+
+
+
+func _on_body_entered(body):
+	if(body.name == "enemyKangaroo"):
+		body.queue_free()
+		queue_free()
--- PracticeGame/ProjectFolder/Nodes/Player/player.gd
+++ PracticeGame/ProjectFolder/Nodes/Player/player.gd
@@ -2,16 +2,24 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
+const fireBallPath = preload("res://Nodes/FireBall.tscn")
 
 func _physics_process(delta):
-
-	var xDir = Input.get_axis("ui_left", "ui_right")
-	var yDir = Input.get_axis("ui_up", "ui_down")
+	var xDir = Input.get_axis("moveLeft", "moveRight")
+	var yDir = Input.get_axis("moveUp", "moveDown")
+	
 	var directions = Vector2(xDir,yDir)
-	if directions:
+	if directions!=Vector2.ZERO:
+		fireBall()
 		velocity = directions * SPEED
 	else:
+		fireBall()		
 		velocity = velocity.move_toward(Vector2(0,0), SPEED)
-
-
+		
 	move_and_slide()
+	
+func fireBall():
+	if ( Input.is_action_just_pressed("FireBall") ) :
+		var fireball = fireBallPath.instantiate() as RigidBody2D
+		fireball.linear_velocity = Vector2(200,200)*get_local_mouse_position().normalized()
+		get_parent().add_child(fireball)
